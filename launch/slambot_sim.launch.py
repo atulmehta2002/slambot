bbcfb890cf2c184d4f32802a32dfacf1e15d0e8d
atulmehta2002 <atulmehta0510@gmail.com>
2024-03-04 04:49:19 +0530

    added sim mode

--- launch/slambot_sim.launch.py
+++ launch/slambot_sim.launch.py
@@ -1,26 +1,35 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch.substitutions import Command
-
-# from launch.actions import RegisterEventHandler
-# from launch.event_handlers import OnProcessExit
 
 
 def generate_launch_description():
 
     package_name='slambot'
     gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
-    world_path = os.path.join(get_package_share_directory(package_name),'worlds','empty.world')    # maze, obstacles, turtlebot, house
-
+    world_path = os.path.join(get_package_share_directory(package_name),'worlds','obstacles.world')    # maze, obstacles, turtlebot, house
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    params = {'use_sim_time': use_sim_time}
 
     robot_state_publisher_launch = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','slambot_rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+    )
+
+    #Create a rviz2 startup node 
+    rviz2_startup_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d' + os.path.join(get_package_share_directory(package_name), 'rviz2_config', 'slambot_sim.rviz')],
+        parameters=[params]
     )
 
     gazebo_startup_node = IncludeLaunchDescription(
@@ -57,7 +66,13 @@ def generate_launch_description():
 
     # Run the node
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use sim time if true'),
+
         robot_state_publisher_launch,
+        rviz2_startup_node,
         gazebo_startup_node,
         spawn_entity,
         twist_mux,
